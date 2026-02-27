9a4f8880bf98d513e2a12112e60c86fea23ff146
jchri772 <chrisju77@uchicago.edu>
2026-02-27 15:29:59 -0600

    update

--- pages/1_Story.py
+++ pages/1_Story.py
@@ -110,11 +110,10 @@ def render_q2_separated(df1, df2):
                 color='Team:N')
 
     chart_1 = (line_1 + points_1).add_params(selection_q2, selection_attack_stats).properties(
-            width=600,
-            height=400,
-            title=alt.TitleParams(text=alt.ExprRef("stat_choice + ' 30-Day Rolling Average " + years[i] + "'"),
-                    fontSize=24))
-    
+        width=800, height=400,
+        title=alt.TitleParams(text=alt.ExprRef("stat_choice + ' 30-Day Rolling Average 2023-2024'"), fontSize=24)
+    )
+
     #2024-2025
     df2_sorted = df2[df2['Date'] >= '2024-07-01'].sort_values('Date')
 
@@ -144,11 +143,10 @@ def render_q2_separated(df1, df2):
                 color='Team:N')
 
     chart_2 = (line_2 + points_2).add_params(selection_q2, selection_attack_stats).properties(
-            width=600,
-            height=400,
-            title=alt.TitleParams(text=alt.ExprRef("stat_choice + ' 30-Day Rolling Average " + years[i] + "'"),
-                    fontSize=24))
-    
+        width=800, height=400,
+        title=alt.TitleParams(text=alt.ExprRef("stat_choice + ' 30-Day Rolling Average 2024-2025'"), fontSize=24)
+    )
+
     st.altair_chart(chart_1, use_container_width=True)
     st.write("---") 
     st.altair_chart(chart_2, use_container_width=True)
