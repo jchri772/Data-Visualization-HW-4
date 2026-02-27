a463411206ac459f2b214a38c6b69ed92db9a4bd
jchri772 <chrisju77@uchicago.edu>
2026-02-27 15:41:37 -0600

    update

--- charts/charts.py
+++ charts/charts.py
@@ -1,6 +1,8 @@
 import altair as alt
 import pandas as pd
 import streamlit as st
+from utils.data_utils import load_data, get_daily_standings, load_home_away_data
+
 
 
 def render(standings_2324, standings_2425):
